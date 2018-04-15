0921720fbbf77ea3057c4f94681047a88086d90c
Philippe V <philippe@arcadian.be>
2018-04-15 14:23:05 +0200

    Configure CI to use the pypi test site

--- ci/pypi_upload.sh
+++ ci/pypi_upload.sh
@@ -3,5 +3,4 @@ set -ex
 
 python -m pip install -Uq twine urllib3[secure]
 ls -la wheelhouse/
-python -m twine upload wheelhouse/*
-
+python -m twine upload --repository-url https://test.pypi.org/legacy/ wheelhouse/*
