405e882da2beb5eb24c6e04da881bf5f3ee963e0
Philippe V <philippe@arcadian.be>
2018-05-03 20:21:32 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='rgeocoder',
-    version='0.1.0',
+    version='0.1.1',
     description="A lightweight offline reverse geocoder implemented in Rust.",
     long_description=readme + '\n\n' + history,
     author="Phil V.",
