22243c33b1e7324b6fa9f2e9088070def128259c
Philippe V <philippe@arcadian.be>
2018-05-04 20:28:10 +0200

    Minor docs improvements
    Switch history to markdown and improve PyPI presentation

---
+++ HISTORY.md
@@ -0,0 +1,5 @@
+History
+-------
+
+### 0.1.2 (2018-05-03) ###
+  * First release on PyPI.
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = []
@@ -29,6 +29,7 @@ setup(
     version='0.1.2',
     description="A lightweight offline reverse geocoder implemented in Rust.",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     author="Phil V.",
     author_email='philippe@arcadian.be',
     url='https://github.com/Phil-V/rgeocoder',
