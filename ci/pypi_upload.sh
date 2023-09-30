113d6f5f7bee3ee5bfeff8ffbc0ebbc956281158
Philippe V <philippe@arcadian.be>
2023-09-30 11:46:09 +0200

    Switch to maturin-generated github CI
    Drop travis and appveyor related files

--- ci/pypi_upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls -la wheelhouse/
-source /tmp/.venv/3.6.5/bin/activate || virtualenv /tmp/.venv/deploy && source /tmp/.venv/deploy/bin/activate
-set -ex
-pip install -Uq twine urllib3[secure]
-python -m twine upload wheelhouse/*
