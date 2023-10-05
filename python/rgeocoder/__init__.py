237de23ca72e1d8ab9d898908209142731af4f01
Philippe V <philippe@arcadian.be>
2023-10-05 14:49:40 +0200

    Bump version: 0.1.4 → 0.1.5

--- python/rgeocoder/__init__.py
+++ python/rgeocoder/__init__.py
@@ -26,7 +26,7 @@ import typing
 from rgeocoder.exceptions import InitializationError as _  # noqa: F401
 from rgeocoder._rgeocoder import RustReverseGeocoder as _RustReverseGeocoder
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __all__ = ['ReverseGeocoder', 'Location']
 
 
